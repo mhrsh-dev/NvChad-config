1f6597446305a73782fe7310fa6a2cdda3e6dcae
r2d2 <matias23571113@gmail.com>
2025-01-01 15:42:02 +0100

    added nvim-dap-ui debugger ui

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -42,10 +42,29 @@ return {
     ft = "python",
     dependencies ={
       "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
     },
     config = function(_, opts)
       local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
       require("dap-python").setup(path)
     end,
   },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function ()
+      local dap = require("dap")
+      local dapui = require("dap-ui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function ()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function ()
+        dapui.close()
+      end
+    end
+  }
 }
